2d6aa6945f97c720f21d495a628479ec5f296157
ayushmishra18013 <ayushmishra0810@gmail.com>
2021-12-25 01:54:56 +0530

    Working on the dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DashBoardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardCardsComponent } from './components/dashboard-cards/dashboard-cards.component';
 
 @NgModule({
   imports: [CommonModule, DashBoardRoutingModule],
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, DashboardCardsComponent],
   providers: [],
 })
 export class DashBoardModule {}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit() {
+    //! for testing alert('login');
+    this.router.navigate(['/dashboard']);
   }
-
 }
